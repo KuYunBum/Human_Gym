06849f0725f8bbc4d242c477ce76a7a7ef92cb2b
CodeName_B <CodeName_B@DESKTOP-JAJ9UUL>
2022-08-03 15:48:47 +0900

    Revert "update"
    This reverts commit 570f4d6c7b3f9cc4cbdaec470546fd607dd2ad14.

--- project/src/main/java/com/spring/project/MyPageController.java
+++ project/src/main/java/com/spring/project/MyPageController.java
@@ -160,7 +160,7 @@ public class MyPageController {
 		System.out.println(ms.chartData(userNum));
 	}
 	
-
+<<<<<<< HEAD
 	@RequestMapping(value = "/record/insert", method = RequestMethod.GET)
 	public void recordInsertGET(int userNum, Model model) throws Exception {
 
@@ -176,7 +176,7 @@ public class MyPageController {
 
 		return "redirect:/user/record/record?userNum=" + userNum;
 	}
-
+=======
 //	루틴
 //	@RequestMapping(value = "/routineBox", method = RequestMethod.GET)
 //	public void routineBox(int userNum, Model model) throws Exception {
@@ -184,6 +184,6 @@ public class MyPageController {
 //	model.addAttribute(ms.routineList(userNum));
 //
 //	}
-
+>>>>>>> refs/remotes/origin/jingyu
 
 }
